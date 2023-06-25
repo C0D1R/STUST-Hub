4b7c85e10f21d3c1299900dcf53744a7b26a161a
C0D1R <gp23456z@gmail.com>
2023-06-26 06:30:30 +0800

    Add link to search time web page

--- js/include_nav_footer.js
+++ js/include_nav_footer.js
@@ -5,6 +5,7 @@
     const navHTML = `
                     <a class="nav__link nav__link--state" href="index.html">首頁</a>
                     <a class="nav__link nav__link--state" href="search.html">課程查詢</a>
+                    <a class="nav__link nav__link--state" href="search_time.html">列表查詢</a>
                     <a class="nav__link nav__link--state" href="club.html">社團一覽</a>
                     <div class="nav__win">
                         <a class="nav__link nav__link--state" href="#">關於作者</a>
