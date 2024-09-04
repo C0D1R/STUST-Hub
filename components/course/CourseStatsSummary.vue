b45b07f80c302b8872f0d3cbd97d0dac537077f4
C0D1R <gp23456z@gmail.com>
2024-09-04 18:47:57 +0800

    Refine CourseStatsSummary component presentation
    - Adjust data presentation format for clarity
    - Remove unused comments and import statements
    - Simplify stats object structure
    - This update streamlines the component and improves code cleanliness

--- components/course/CourseStatsSummary.vue
+++ components/course/CourseStatsSummary.vue
@@ -6,14 +6,6 @@
             v-for="stat in stats"
             class="flex gap-x-2 sm:gap-x-4 items-end justify-center first-of-type:border-r first-of-type:border-gray-100/15"
         >
-            <!-- <CountTo
-                :start-val="0"
-                :end-val=""
-                :duration="1000"
-                :separator="null"
-                :suffix="stat.suffix"
-                class="text-2xl font-bold"
-            /> -->
             <span class="text-2xl font-bold">{{ stat.count }}</span>
             <span class="text-sm text-gray-100/55">
                 {{ stat.text }}
@@ -23,19 +15,14 @@
 </template>
 
 <script setup>
-// import CountToPackage from "vue3-count-to";
-// const { CountTo } = CountToPackage;
-
 const stats = {
     department: {
-        count: 25,
+        count: "25",
         text: "個科系",
-        suffix: "",
     },
     courseCount: {
-        count: 3700,
+        count: "3700+",
         text: "門課程",
-        suffix: "+",
     },
 };
 </script>
