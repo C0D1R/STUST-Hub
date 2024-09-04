e9eeef2df383363ba61c28c9bc33065e401176e5
C0D1R <gp23456z@gmail.com>
2024-09-04 18:38:38 +0800

    Refactor CourseStatsSummary component to remove vue3-count-to
    - Remove CountTo component usage from template
    - Display static numbers for department and course counts
    - This change improves compatibility and simplifies the component

--- components/course/CourseStatsSummary.vue
+++ components/course/CourseStatsSummary.vue
@@ -6,14 +6,15 @@
             v-for="stat in stats"
             class="flex gap-x-2 sm:gap-x-4 items-end justify-center first-of-type:border-r first-of-type:border-gray-100/15"
         >
-            <CountTo
+            <!-- <CountTo
                 :start-val="0"
-                :end-val="stat.count"
+                :end-val=""
                 :duration="1000"
                 :separator="null"
                 :suffix="stat.suffix"
                 class="text-2xl font-bold"
-            />
+            /> -->
+            <span class="text-2xl font-bold">{{ stat.count }}</span>
             <span class="text-sm text-gray-100/55">
                 {{ stat.text }}
             </span>
@@ -22,8 +23,8 @@
 </template>
 
 <script setup>
-import CountToPackage from "vue3-count-to";
-const { CountTo } = CountToPackage;
+// import CountToPackage from "vue3-count-to";
+// const { CountTo } = CountToPackage;
 
 const stats = {
     department: {
