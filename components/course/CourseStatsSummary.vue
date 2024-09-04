c6eefd51ce230da8947a64f23e668bbc62828bca
C0D1R <gp23456z@gmail.com>
2024-09-04 16:36:21 +0800

    Add CourseStatsSummary component for displaying course statistics
    - Create new CourseStatsSummary.vue component
    - Use vue3-count-to for animated number counting effect
    - Display total number of departments and courses
    - This addition provides users with a quick overview of course offerings

---
+++ components/course/CourseStatsSummary.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="grid grid-cols-2 gap-y-2 py-2 border border-gray-100/15 rounded-lg"
+    >
+        <p
+            v-for="stat in stats"
+            class="flex gap-x-2 sm:gap-x-4 items-end justify-center first-of-type:border-r first-of-type:border-gray-100/15"
+        >
+            <CountTo
+                :start-val="0"
+                :end-val="stat.count"
+                :duration="1000"
+                :separator="null"
+                :suffix="stat.suffix"
+                class="text-2xl font-bold"
+            />
+            <span class="text-sm text-gray-100/55">
+                {{ stat.text }}
+            </span>
+        </p>
+    </div>
+</template>
+
+<script setup>
+import { CountTo } from "vue3-count-to";
+
+const stats = {
+    department: {
+        count: 25,
+        text: "個科系",
+        suffix: "",
+    },
+    courseCount: {
+        count: 3700,
+        text: "門課程",
+        suffix: "+",
+    },
+};
+</script>
