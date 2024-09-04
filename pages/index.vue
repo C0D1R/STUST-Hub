deb3d865dde312874e7c0f6bba017e8648999838
C0D1R <gp23456z@gmail.com>
2024-09-04 16:34:25 +0800

    Update index page content and layout
    - Refine introductory text for STUST HUB
    - Comment out reference to club information
    - Implement CourseStatsSummary component for course statistics display
    - This update improves the landing page's clarity and focus on course information

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <template>
-    <div>
-        <!-- <h1>Color mode: {{ $colorMode.value }}</h1>
-        <select v-model="$colorMode.preference">
-            <option value="system">System</option>
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
-            <option value="sepia">Sepia</option>
-        </select> -->
-
-        <h1>STUST HUB</h1>
-        <p>STUST HUB，一個由學生自製的非官方工具</p>
-        <p>你能夠尋找到南臺科技大學各科系的最新課程資訊</p>
-        <p>一覽校園裡的所有社團</p>
+    <div class="">
+        <p class="grid py-2">
+            <span>STUST HUB</span>
+            <span>是一個由學生自製的非官方工具</span>
+            <span>你可以在這裡找到南臺科技大學各科系的最新課程資訊</span>
+        </p>
+        <!-- <p>也可以一覽校園裡的所有社團</p> -->
+        <CourseStatsSummary />
     </div>
 </template>
