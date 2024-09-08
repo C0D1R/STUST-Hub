2ad2e46432197140f09cb19e821986f715e22ec6
C0D1R <gp23456z@gmail.com>
2024-09-08 17:20:36 +0800

    Add course selection info page
    - Create new page for displaying course selection timeline and information
    - Implement CourseSelectionTimeline component in the page
    - Set up page meta and title for SEO optimization
    - Improve user access to important course selection dates and details
    
    This addition provides students with a centralized location for course selection information.

---
+++ pages/course/[year]/[sem]/SelectionInfo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <CourseSelectionTimeline />
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    path: "/course/:year/:sem/selection-info",
+});
+
+useHead({
+    title: "選課資訊 | STUST HUB",
+});
+</script>
