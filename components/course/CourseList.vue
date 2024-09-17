081be216dc048bc03a32f1bcb94a1c5820bd5df0
C0D1R <gp23456z@gmail.com>
2024-09-17 21:00:56 +0800

    Refactor CourseList component and implement strategy pattern
    - Move fetchCourses and filterCourses methods to separate strategy files
    - Enable CourseList component to use different strategies for various pages
    - Improve flexibility in displaying different course information
    - Remove <CourseListViewSwitcher /> from CourseList component
    - Allow more freedom in layout design by moving view switcher to individual pages
    
    This refactoring enhances component reusability and layout customization options.

--- components/course/CourseList.vue
+++ components/course/CourseList.vue
@@ -1,7 +1,5 @@
 <template>
     <div v-if="courses && courses.length">
-        <CourseListViewSwitcher />
-
         <div @change="toggleFavorite" :class="styles.getCourseContainer()">
             <CourseListCaption />
 
@@ -17,6 +15,7 @@
                     <h2 :class="styles.getCourseName()">
                         {{ course.name }}
                     </h2>
+
                     <p
                         v-for="item in displayItems"
                         :key="`${course.code}${item.key}`"
@@ -53,235 +52,22 @@ const courseFilterStore = useCourseFilterStore();
 const { courseFilterParams } = storeToRefs(courseFilterStore);
 
 const courseSemesterStore = useCourseSemesterStore();
-const { year, sem, semester } = storeToRefs(courseSemesterStore);
+const { semester } = storeToRefs(courseSemesterStore);
 
-const { setItem, getItem } = useLocalforage();
-
-const fetchCourses = async () => {
-    const targetDepartmentKey = `${semester.value}_${courseFilterParams.value.department}`;
-    const targetGeneralKey = `${semester.value}_general`;
-
-    try {
-        const cachedCourse = (await getItem(targetDepartmentKey)).filter(
-            (c) => c !== null
-        );
-        const cachedGeneralCourse = (await getItem(targetGeneralKey)).filter(
-            (c) => c !== null
-        );
-        if (cachedCourse && cachedGeneralCourse) {
-            console.log("courses from cache");
-            return [cachedCourse, cachedGeneralCourse];
-        }
-    } catch (err) {
-        console.error(`Error reading from cache: ${err}`);
-    }
-
-    try {
-        const courseData = await $fetch(
-            `/api/course/${year.value}/${sem.value}`
-        );
-
-        try {
-            if (courseData) {
-                for (const dept of courseData) {
-                    await setItem(dept.id, dept.courses);
-                }
-            }
-        } catch (err) {
-            console.error(`Error setting item to cache: ${err}`);
-        }
-
-        console.log("courses from api");
-        const departmentCourses = (await getItem(targetDepartmentKey)).filter(
-            (c) => c !== null
-        );
-        const generalCourses = (await getItem(targetGeneralKey)).filter(
-            (c) => c !== null
-        );
-        return [departmentCourses, generalCourses];
-    } catch (err) {
-        console.error(`Error fetching courses: ${err}`);
-        throw err;
-    }
-};
-
-const getGeneralField = (dept) => {
-    const scienceDepts = [
-        "ee",
-        "mech",
-        "eecs",
-        "oe",
-        "csie",
-        "chem",
-        "bio",
-        "ic",
-        "vc",
-        "mes",
-        "cpd",
-        "pmi",
-    ];
-    const humanitiesDepts = [
-        "imi",
-        "ib",
-        "fin",
-        "ba",
-        "mis",
-        "accinfo",
-        "leisure",
-        "mim",
-        "hm",
-        "english",
-        "japan",
-        "childcare",
-        "ss",
-    ];
-    const type = {
-        science: ["人文藝術領域", "社會科學領域", "綜合實踐領域"],
-        humanities: ["人文藝術領域", "自然科學領域", "綜合實踐領域"],
-        all: ["人文藝術領域", "社會科學領域", "自然科學領域", "綜合實踐領域"],
-    };
-    return scienceDepts.includes(dept)
-        ? type.science
-        : humanitiesDepts.includes(dept)
-        ? type.humanities
-        : type.all;
-};
-
-const filterCourses = ([deptCourses, genCourses]) => {
-    if (!deptCourses) return [];
-
-    const typeMap = {
-        compulsory: "必修",
-        elective: "選修",
-        general: "通識",
-    };
-
-    const typePattern = courseFilterParams.value.courseType
-        .map((type) => typeMap[type])
-        .join("|");
-
-    const typeFilterRegex = new RegExp(`(${typePattern})`);
-
-    const gradeMap = {
-        first: "一",
-        second: "二",
-        third: "三",
-        fourth: "四",
-        fifth: "五",
-    };
-
-    const gradePattern = courseFilterParams.value.grade
-        .map((grade) => gradeMap[grade])
-        .join("|");
-
-    const gradeFilterRegex = new RegExp(`(?<!^)(${gradePattern})`);
-
-    const times = {};
-    const dayMap = {
-        M: "一",
-        T: "二",
-        W: "三",
-        R: "四",
-        F: "五",
-        S: "六",
-        U: "日",
-    };
-
-    courseFilterParams.value.time.forEach((time) => {
-        const splitRegex = new RegExp(/(?<=[A-Za-z])(?=\d+)/);
-        const [day, period] = time.split(splitRegex);
-
-        if (!times[day]) {
-            times[day] = [];
-        }
-        times[day].push(period);
-    });
-
-    const timePattern = Object.entries(times)
-        .map(([day, periods]) => {
-            const mappedDay = dayMap[day];
-            const joinedPeriods = periods.join("|");
-            const dayPattern = `(?<=${mappedDay})[\\s\\d]+\\b(${joinedPeriods})\\b`;
-
-            return dayPattern;
-        })
-        .join("|");
-
-    const timeFilterRegex = new RegExp(timePattern);
-
-    const coursesFiltered = deptCourses
-        .filter((course) => {
-            // Filter by course type
-            return typeFilterRegex.test(course.type);
-        })
-        .filter((course) => {
-            // Filter by grade
-            // Regex: (?<!^)(一|二|三|四|五)
-
-            const isGradeMatch = course.classes.some((classItem) =>
-                gradeFilterRegex.test(classItem)
-            );
-
-            return isGradeMatch;
-        })
-        .filter((course) => {
-            // Filter by time
-            // Regex: (?<=一)[\s\d]+\b(1|2|3|4|5|6|7|8|9|10|11|12|13|14)\b
-            // Regex: (?<=二)[\s\d]+\b(1|2|3|4|5|6|7|8|9|10|11|12|13|14)\b
-            // Regex: (?<=三)[\s\d]+\b(1|2|3|4|5|6|7|8|9|10|11|12|13|14)\b
-            // Regex: (?<=四)[\s\d]+\b(1|2|3|4|5|6|7|8|9|10|11|12|13|14)\b
-            // Regex: (?<=五)[\s\d]+\b(1|2|3|4|5|6|7|8|9|10|11|12|13|14)\b
-            // courseFilterParams.value.time;
-
-            return timeFilterRegex.test(course.schedule);
-        })
-        .filter((course) => {
-            // Filter by school system
-            // courseFilterParams.value.schoolSystem;
-
-            return true;
-        })
-        .map((course) => {
-            // Filter by class
-            course.classes =
-                course.classes.length > 1
-                    ? `${course.classes[0].trim()}等合開`
-                    : course.classes[0];
-            return course;
-        });
-
-    const isGeneral = courseFilterParams.value.courseType.some(
-        (type) => type === "general"
-    );
-    const fieldTypePattern = getGeneralField(
-        courseFilterParams.value.department
-    ).join("|");
-    const fieldTypeRegex = new RegExp(`${fieldTypePattern}`);
-
-    const generalCoursesFiltered = isGeneral
-        ? genCourses
-              .filter((course) =>
-                  course.classes.some((field) => fieldTypeRegex.test(field))
-              )
-              .filter((course) => timeFilterRegex.test(course.schedule))
-              .map((course) => {
-                  course.classes = course.classes.toString();
-                  return course;
-              })
-        : [];
-
-    return [coursesFiltered, generalCoursesFiltered];
-};
+const props = defineProps({
+    strategy: {
+        type: Object,
+        required: true,
+    },
+});
 
 watch(
     [semester, courseFilterParams],
     async () => {
-        const [departmentCourses, generalCourses] = await fetchCourses();
-        const [deptCoursesFiltered, genCoursesFiltered] = filterCourses([
-            departmentCourses,
-            generalCourses,
-        ]);
-        courses.value = [...deptCoursesFiltered, ...genCoursesFiltered];
+        const coursesData = await props.strategy.fetchCourses();
+        const coursesFiltered = props.strategy.filterCourses(coursesData);
+        const coursesProcessed = props.strategy.processCourses(coursesFiltered);
+        courses.value = coursesProcessed;
     },
     {
         immediate: true,
@@ -350,7 +136,7 @@ const styles = computed(() => ({
     getCourseLink: () => {
         const baseStyle = ["grid"];
         const cardStyle = [
-            "col-span-3 row-span-3",
+            "col-span-full row-span-full",
             "grid-cols-[1.3fr,1fr,1fr] grid-rows-3 gap-x-1.5 gap-y-2",
             "md:grid-cols-[1.5fr,1fr,1fr]",
         ];
@@ -363,7 +149,7 @@ const styles = computed(() => ({
     getCourseName: () => {
         const baseStyle = ["overflow-hidden text-ellipsis whitespace-nowrap"];
         const cardStyle = [
-            "col-span-3 border-b border-black/15 py-2 font-bold",
+            "col-span-full border-b border-black/15 py-2 font-bold",
             "dark:border-gray-100/15",
         ];
         const tableStyle = [""];
