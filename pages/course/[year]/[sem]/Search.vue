bbf17addf095bcd9ec9a6e7517dfb244fd116c5a
C0D1R <gp23456z@gmail.com>
2024-09-17 21:40:25 +0800

    Enhance course search page with mode switching and enrollment list
    - Add courseListCurrentMode ref to manage display mode
    - Implement handleModeChange function for mode switching
    - Integrate CourseListViewSwitcher component
    - Add conditional rendering for CourseList and CourseEnrollmentList
    - Use courseSearchPageStrategy for CourseList component
    - Improve user experience by providing different course view options
    
    This change allows users to switch between basic course information and real-time enrollment data views.

--- pages/course/[year]/[sem]/Search.vue
+++ pages/course/[year]/[sem]/Search.vue
@@ -1,6 +1,30 @@
 <template>
     <div>
         <CourseFilterForm />
-        <CourseList />
+
+        <div class="mb-4 grid gap-y-2 sm:grid-cols-2 sm:gap-x-4">
+            <!-- <CourseListModeSwitcher @modeChange="handleModeChange" /> -->
+            <CourseListViewSwitcher />
+        </div>
+
+        <CourseList
+            v-if="courseListCurrentMode === 'basic'"
+            :strategy="courseSearchPageStrategy"
+        />
+        <CourseEnrollmentList v-else />
     </div>
 </template>
+
+<script setup>
+useHead({
+    title: "搜尋課程 | STUST HUB",
+});
+
+const courseSearchPageStrategy = useCourseSearchPageStrategy();
+
+const courseListCurrentMode = ref("basic");
+
+const handleModeChange = (mode) => {
+    courseListCurrentMode.value = mode;
+};
+</script>
