ef80b68bf87b4a8d1fc27accfc00b7a6c4e77f40
C0D1R <gp23456z@gmail.com>
2024-09-22 04:29:25 +0800

    Add CourseCategoryLink component for course exploration pages
    - Create new CourseCategoryLink.vue component
    - Implement reusable link structure for course categories
    - Use component in Class, Language, and Instructor pages
    - Improve consistency in link appearance across pages
    - Enhance modularity and maintainability of the codebase
    
    This change introduces a standardized way to display category links in course exploration pages, improving code reusability and user interface consistency.

---
+++ components/course/CourseCategoryLink.vue
@@ -0,0 +1,23 @@
+<template>
+    <NuxtLink :to="to" class="block bg-gray-800 p-4 rounded-md">
+        <h2 class="font-bold text-lg">{{ text }}</h2>
+        <p class="text-sm text-gray-100/55">{{ description }}</p>
+    </NuxtLink>
+</template>
+
+<script setup>
+const props = defineProps({
+    to: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+});
+</script>
