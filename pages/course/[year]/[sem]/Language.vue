74772c425664b9f7d5c79646bc3cb6bfcadf0277
C0D1R <gp23456z@gmail.com>
2024-09-22 04:27:25 +0800

    Implement Class, Language, and Instructor exploration pages
    - Add functional content to Class.vue, Language.vue, and Instructor.vue
    - Add SEO metadata for each page
    - Create dynamic links based on available options
    - Improve user navigation and course exploration experience
    
    This change enables users to browse and select specific classes, languages, and instructors, enhancing the overall functionality of the course exploration feature.

--- pages/course/[year]/[sem]/Language.vue
+++ pages/course/[year]/[sem]/Language.vue
@@ -1,3 +1,44 @@
 <template>
-    <div class="sm:text-2xl">此頁面將在不久後的將來啟用！</div>
+    <div>
+        <ul
+            class="grid grid-cols-1 gap-2 mb-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        >
+            <li v-for="link in links">
+                <CourseCategoryLink
+                    :to="link.to"
+                    :text="link.text"
+                    :description="link.description"
+                />
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script setup>
+useSeoMeta({
+    title: `語言探索 | STUST HUB`,
+    ogTitle: `語言探索 | STUST HUB`,
+    description: `立即查看 STUST HUB 提供的語言探索頁面！在這裡，您可以快速找到南台科技大學 ${route.params.year} 學年第 ${route.params.sem} 學期所有課程所使用到的語言。`,
+    ogDescription: `立即查看 STUST HUB 提供的語言探索頁面！在這裡，您可以快速找到南台科技大學 ${route.params.year} 學年第 ${route.params.sem} 學期所有課程所使用到的語言。`,
+});
+
+const languages = ref([
+    "華語",
+    "英語",
+    "◎全程外語教學",
+    "台語",
+    "其他",
+    "日語",
+    "西班牙語",
+]);
+
+const route = useRoute();
+console.log(route);
+const links = computed(() =>
+    languages.value.map((language) => ({
+        text: language,
+        description: `查看採用${language}授課的相關課程`,
+        to: `${route.path}/${language}`,
+    }))
+);
+</script>
