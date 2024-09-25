b5b06ccb9023d0459951c3eb57b88d8721676ce9
C0D1R <gp23456z@gmail.com>
2024-09-26 04:21:05 +0800

    Refine homepage content and SEO description
    - Adjust SEO meta description to better reflect the platform's purpose
    - Ensure consistency between visual content and meta description
    
    This change enhances the homepage's visual appeal and improves its search engine optimization by aligning the visible content with the meta description.

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,24 @@
 <template>
     <div>
-        <p
-            class="flex flex-wrap gap-x-4 gap-y-2 py-8 justify-center mb-4 sm:gap-x-6"
-        >
+        <p class="flex flex-wrap gap-4 py-8 justify-center mb-4 sm:gap-x-6">
             <span class="text-5xl font-bold text-center">STUST HUB</span>
-            <span class="flex gap-x-2 justify-center items-center">
+            <span class="grid grid-cols-2 gap-2 place-items-center sm:flex">
                 <!-- grid grid-cols-2 gap-x-2 -->
                 是由
                 <span
                     class="text-xl font-bold p-2 bg-gray-800 rounded-lg sm:px-3 md:px-4"
                 >
-                    學生
+                    南台科大學生
                 </span>
-                自製的
+                建立的
                 <span
                     class="text-xl font-bold p-2 bg-gray-800 rounded-lg sm:px-3 md:px-4"
                 >
-                    非官方工具
+                    選課資訊平台
                 </span>
             </span>
         </p>
-        <!-- <p>也可以一覽校園裡的所有社團</p> -->
+
         <CourseStatsSummary />
     </div>
 </template>
@@ -34,8 +32,8 @@ useSeoMeta({
     title: "STUST HUB",
     ogTitle: "STUST HUB",
     description:
-        "STUST HUB 是由南台科大學生建立的資訊平台，提供南台科技大學裡各科系的最新課程資料與選課資訊。",
+        "STUST HUB 是由南台科大學生建立的資訊平台，提供南台科技大學各科系的最新課程資料與選課資訊，讓你的選課過程更加流暢。",
     ogDescription:
-        "STUST HUB 是由南台科大學生建立的資訊平台，提供南台科技大學裡各科系的最新課程資料與選課資訊。",
+        "STUST HUB 是由南台科大學生建立的資訊平台，提供南台科技大學各科系的最新課程資料與選課資訊，讓你的選課過程更加流暢。",
 });
 </script>
